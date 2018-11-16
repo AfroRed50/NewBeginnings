13f31b2a6933a19943aa689cd38ebc86ff37cbd3
Wesley Hobbs <43051794+AfroRed50@users.noreply.github.com>
2018-11-16 08:39:19 -0500

    Upload
    Added second button function.

--- button.js
+++ button.js
@@ -1,7 +1,16 @@
 /* ==== More/Less Button Function ==== */
 
-function buttonClick() {
+function buttonClick1() {
     var x = document.getElementById("shrink");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function buttonClick2() {
+    var x = document.getElementById("address_shrink");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
